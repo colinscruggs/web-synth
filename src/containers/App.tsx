2f9846b44ab5253b7547013d31886575e93cd139
Colin Scruggs <colinscruggs97@gmail.com>
2021-10-10 16:59:32 -0500

    first try w/ 'sequencing', mixed success

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { isParameter } from "typescript";
 
 import { Oscillator } from "../components/Oscillator";
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
+import context from "../hooks/context";
+// import Slider from "rc-slider";
+// import "rc-slider/assets/index.css";
 import "../styles/App.css";
 
 export const App = () => {
@@ -21,6 +23,76 @@ export const App = () => {
     'A#4': 466.16,
     'B4': 493.88,
     'C5': 523.25
+  };
+
+  const { audioContext } = useContext(context);
+  let attackTime = 0.3;
+  let sustainLevel = 0.5;
+  let releaseTime = 0.2;
+  
+  const [playingSequence, setPlayingSequence] = useState(false);
+  const [currentNotes, setCurrentNotes] = useState();
+  const [currentSequence, setCurrentSequence] = useState(['C4', 'D4', 'E4']);
+  const [currentNoteIndex, setCurrentNoteIndex] = useState(0);
+
+  // establish current playing notes as all false
+  useEffect(() => {
+    const notesObject = {} as any;
+    for (const note in notes) {
+      notesObject[note] = false;
+    }
+    setCurrentNotes(notesObject);
+  }, [])
+
+  // useEffect(() => {
+  //   if (playingSequence) {
+  //     noteLoop();
+  //   }
+  // }, [playingSequence])
+
+  const playCurrentNote = (note: string) => {
+    const oscillator = audioContext.createOscillator();
+    const gain = audioContext.createGain();
+
+    gain.gain.setValueAtTime(0, 0);
+    gain.gain.linearRampToValueAtTime(sustainLevel, audioContext.currentTime + 0.5 * attackTime);
+    gain.gain.setValueAtTime(sustainLevel, audioContext.currentTime + 0.5 - 0.5 * releaseTime);
+    gain.gain.linearRampToValueAtTime(0, audioContext.currentTime + 0.5);
+    gain.connect(audioContext.destination);
+
+    // @ts-ignore
+    oscillator.frequency.value = notes[note];
+    oscillator.type = 'sine' as OscillatorType;
+    oscillator.start(0);
+    oscillator.stop(audioContext.currentTime + 1);
+    oscillator.connect(gain);
+
+    nextNote();
+  }
+
+  const nextNote = () => {
+    let currentIndex = currentNoteIndex;
+
+    if (currentIndex === 2) {
+      currentIndex = 0;
+    } else {
+      currentIndex = currentIndex++;
+    }
+    setCurrentNoteIndex(currentIndex);
+  }
+
+  const noteLoop = () => {
+    setInterval(() => {
+      currentSequence.forEach((note, index) => {
+        setTimeout(() => {
+          playCurrentNote(note);
+        }, 500 * index)
+    })
+    }, currentSequence.length * 500)
+  }
+
+  const setPlaying = (val: boolean) => {
+    setPlaying(val);
   }
 
   return (
@@ -28,15 +100,8 @@ export const App = () => {
     <header className="App-header">
       WebSynth
     </header>
-    <div className="keyboard">
-      {Object.entries(notes).map(note => {
-        const noteName = note[0];
-        const freq = note[1];
-        return (
-          <Oscillator frequency={freq} note={noteName} waveform={"sine"} />
-        )
-      })}
-    </div>
+    <button onClick={() => setPlaying(true)}>START</button>
+    <button onClick={() => setPlaying(false)}>STOP</button>
   </div>
   );
 };
--- src/hooks/useAudioContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-const context = createContext({
-  audioContext: new AudioContext()
-});
-
-console.log("Create AudioContext instance");
-
-export default () => useContext(context);
